bbcf71bbfcb0959e53acae390e68fb443cbbabc1
AbidHasan0 <abidhasanrazvi@gmail.com>
2022-05-16 04:38:37 +0600

    add bike route created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import AddBike from './Pages/AddBike/AddBike';
 import AllProducts from './Pages/AllProducts/AllProducts';
 import BikeDetails from './Pages/BikeDetails/BikeDetails';
 import Blogs from './Pages/Blogs/Blogs';
@@ -22,6 +23,7 @@ function App() {
         <Route path='/register' element={<Register></Register>}></Route>
         <Route path='/products' element={<RequireAuth><AllProducts></AllProducts></RequireAuth>}></Route>
         <Route path='/bikeDetails/:bikesId' element={<RequireAuth><BikeDetails></BikeDetails></RequireAuth>}></Route>
+        <Route path='/addbike' element={<RequireAuth><AddBike></AddBike></RequireAuth>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
 
       </Routes>
